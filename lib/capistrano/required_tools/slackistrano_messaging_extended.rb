7e5e338826252ef98aff470b38b58bd00722f62a
Pascal Birchler <hello@pascalbirchler.ch>
2017-06-06 13:35:56 +0200

    Add compare link if available

--- lib/capistrano/required_tools/slackistrano_messaging_extended.rb
+++ lib/capistrano/required_tools/slackistrano_messaging_extended.rb
@@ -22,21 +22,54 @@ module Capistrano
       end
 
       # Get current revision
-      def revision(default = 'Unknown')
-        fetch(:current_revision, default)
+	  def current_revision
+	    fetch(:current_revision)
+	  end
+
+	  # Get previous revision
+      def previous_revision
+        fetch(:previous_revision)
+      end
+
+      # Get individual parts of the repo_url
+      def repo
+        repo_url.match(/(git@|https:\/\/)(?<host>([\w\.@]+))(\/|:)(?<owner>[\w,\-,\_]+)\/(?<repo>[\w,\-,\_]+)(.git){0,1}((\/){0,1})/)
+      end
+
+      # Get information about the revision being deployed
+      def revision
+        if repo.nil? || current_revision.nil?
+          'Unknown'
+        elsif previous_revision.nil?
+          current_revision_url
+        else
+          revision_compare_url
+        end
       end
 
       # Get current revision URL
-	  def revision_url
-	    repo = repo_url.match(/(git@|https:\/\/)(?<host>([\w\.@]+))(\/|:)(?<owner>[\w,\-,\_]+)\/(?<repo>[\w,\-,\_]+)(.git){0,1}((\/){0,1})/)
-        repo.nil? ? revision : "<https://%{host}/%{owner}/%{repo}/%{commit}/%{revision}|%{revision_short}>" % {
-          :host => repo[:host],
-          :owner => repo[:owner],
-          :repo => repo[:repo],
-          :commit => repo[:host] == 'bitbucket.org' ? 'commits' : 'commit',
-          :revision => revision,
-          :revision_short => revision[0..10]
-        }
+	  def current_revision_url
+        "<https://%{host}/%{owner}/%{repo}/%{commit}/%{revision}|%{revision_short}>" % {
+		  :host => repo[:host],
+		  :owner => repo[:owner],
+		  :repo => repo[:repo],
+		  :commit => repo[:host] == 'bitbucket.org' ? 'commits' : 'commit',
+		  :revision => current_revision,
+		  :revision_short => current_revision[0..10]
+		}
+      end
+
+      # Get revision comparison
+	  def revision_compare_url
+        "<https://%{host}/%{owner}/%{repo}/%{compare}/%{previous_revision}...%{current_revision}|%{revision_short}>" % {
+		  :host => repo[:host],
+		  :owner => repo[:owner],
+		  :repo => repo[:repo],
+		  :compare => repo[:host] == 'bitbucket.org' ? 'branches/compare' : 'compare',
+		  :current_revision => current_revision,
+          :previous_revision => previous_revision,
+          :revision_short => previous_revision[0..7] + '...' + current_revision[0..7]
+		}
       end
 
       # More detailed updated message.
@@ -59,7 +92,7 @@ module Capistrano
             short: true
           }, {
             title: 'Revision',
-            value: revision_url,
+            value: revision,
             short: true
           }],
           fallback: super[:text]
