2d3dfbd1affaf69f96c793e4e5c55acca20b1c60
Dominik Schilling <dominikschilling+git@gmail.com>
2017-09-14 17:10:15 +0200

    Slackistrano: Bring support for icon_url, icon_emoji, and username to the new extended class
    See #2, #7.

--- lib/capistrano/required_tools/slackistrano_messaging_extended.rb
+++ lib/capistrano/required_tools/slackistrano_messaging_extended.rb
@@ -3,6 +3,14 @@ require 'slackistrano/capistrano'
 module Capistrano
   module RequiredTools
     class SlackistranoMessagingExtended < Slackistrano::Messaging::Base
+
+      def initialize(env: nil, team: nil, channel: nil, token: nil, webhook: nil, icon_url: nil, icon_emoji: nil, username: nil)
+        super(env: env, team: team, channel: channel, token: token, webhook: webhook)
+        @icon_url = icon_url
+        @icon_emoji = icon_emoji
+        @username = username
+      end
+
       # Suppress updating message.
       def payload_for_updating
         nil
@@ -19,6 +27,26 @@ module Capistrano
 
       def payload_for_failed
         make_message(super.merge(color: 'danger'))
+      end
+
+      def icon_url
+        if @icon_url.nil?
+          super
+        else
+          @icon_url
+        end
+      end
+
+      def icon_emoji
+        @icon_emoji
+      end
+
+      def username
+       if @username.nil?
+         super
+       else
+         @username
+       end
       end
 
       # Get current revision
