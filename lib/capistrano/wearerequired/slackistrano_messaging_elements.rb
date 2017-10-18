bf8dab2d26c883d5f1c4f8601c1e3799d19fbaf7
Ulrich Pogson <grapplerulrich@gmail.com>
2017-10-18 14:17:18 +0300

    Override deployer helper function to use git name
    #17

--- lib/capistrano/wearerequired/slackistrano_messaging_elements.rb
+++ lib/capistrano/wearerequired/slackistrano_messaging_elements.rb
@@ -31,6 +31,14 @@ module Capistrano
        end
       end
 
+      # Override the deployer helper to pull the best name available (git, password file, env vars).
+      def deployer
+        name = `git config user.name`.strip.split.first
+        name = nil if name.empty?
+        name ||= Etc.getpwnam(ENV['USER']).gecos || ENV['USER'] || ENV['USERNAME']
+        name
+      end
+
     end
   end
 end
