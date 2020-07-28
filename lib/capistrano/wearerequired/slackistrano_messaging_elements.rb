f2a10e8e95a8f60b6abe08867cde3a118f00a56a
Ulrich Pogson <grapplerulrich@gmail.com>
2020-07-28 10:16:58 +0200

    Switch to single argument to match parent class
    Fixes #34
    
    username, icon_emoji & icon_url can be overridden without a custom class https://github.com/phallstrom/slackistrano/issues/68

--- lib/capistrano/wearerequired/slackistrano_messaging_elements.rb
+++ lib/capistrano/wearerequired/slackistrano_messaging_elements.rb
@@ -4,31 +4,8 @@ module Capistrano
   module Wearerequired
     class SlackistranoMessagingElements < Slackistrano::Messaging::Base
 
-      def initialize(env: nil, team: nil, channel: nil, token: nil, webhook: nil, icon_url: nil, icon_emoji: nil, username: nil)
-        super(env: env, team: team, channel: channel, token: token, webhook: webhook)
-        @icon_url = icon_url
-        @icon_emoji = icon_emoji
-        @username = username
-      end
-
-      def icon_url
-        if @icon_url.nil?
-          super
-        else
-          @icon_url
-        end
-      end
-
-      def icon_emoji
-        @icon_emoji
-      end
-
-      def username
-       if @username.nil?
-         super
-       else
-         @username
-       end
+      def initialize(options = {})
+        super(options)
       end
 
       # Override the deployer helper to pull the best name available (git, password file, env vars).
